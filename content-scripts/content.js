7c823ccfc74c07eb0529588072ffad602731e968
jienhui <enhui_ji@qq.com>
2022-06-27 17:24:17 +0800

    支持自定义

--- content-scripts/content.js
+++ content-scripts/content.js
@@ -2,6 +2,14 @@ console.log('i am chameleon.');
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     console.log(request);
+    if (request.action == 'loadDocument') {
+        const list = []
+        workOnEle($, $("body"), list, "body");
+        sendResponse({
+            list: list,
+            title: $("title").text().trim(),
+        });
+    }
     if (request.hiddenDom) {
         handleHiddenDom(request.hiddenDom, request.host)
     }
@@ -53,3 +61,61 @@ function handleStyle(style, host) {
         $('head').append(styleTag)
     }
 }
+
+// 解析并递归子节点，输出图文
+const workOnEle = function ($, ele, list, seletor, ref) {
+    // console.log(ele)
+    const current = $(ele);
+    if (["script", "style"].includes(ele.tagName)) {
+        return;
+    }
+    if (["img", "video"].includes(ele.tagName)) {
+        // console.log(current)
+        const src = current.attr("src") || current.attr("data-src");
+        if (src) {
+            list.push({
+                type: ele.tagName,
+                content: src,
+                selector: seletor,
+                insideId: "e" + (list.length + 1),
+            });
+        }
+        return;
+    }
+    if (ele.tagName == "a") {
+        // console.log(current)
+        ref = current.attr("href");
+    }
+    const count = list.length;
+    current.children().each((index, child) => {
+        // console.log(child)
+        if (child.children.length > 0) {
+            workOnEle(
+                $,
+                child,
+                list,
+                seletor + ">" + child.tagName + ":nth-child(" + (index + 1) + ")",
+                ref
+            );
+        } else {
+            if (child.innerText.trim()) {
+                list.push({
+                    type: "text",
+                    content: child.innerText.trim(),
+                    selector: seletor,
+                    ref: ref,
+                    insideId: "e" + (list.length + 1),
+                });
+            }
+        }
+    });
+    if (list.length == count && ele.innerText.trim()) {
+        list.push({
+            type: "text",
+            content: ele.innerText.trim(),
+            selector: seletor,
+            ref: ref,
+            insideId: "e" + (list.length + 1),
+        });
+    }
+};
--- popup/popup.js
+++ popup/popup.js
@@ -2,129 +2,214 @@ document.addEventListener('DOMContentLoaded', function () {
     const vue = new Vue({
         data: {
             items: [],
-            refreshItems: false
+            refreshItems: false,
+            currentNav: 'mine',
+            page: {
+                list: [],
+                title: ''
+            },
         },
         computed: {},
         render: function (h) {
             const _this = this;
-            return h(
-                'div', {
+            const contentMine = h('div', {
+                class: 'content-mine h-full'
+            }, [
+                h('div', {
                     attrs: {
-                        class: 'w-60 h-80 pb-1 body'
+                        class: 'flex items-center justify-between p-2 overflow-hidden'
                     }
-                },
-                [
+                }, [
                     h('div', {
                         attrs: {
-                            class: 'flex items-center justify-between p-2 overflow-hidden'
+                            class: 'bg-green-50'
+                        }
+                    }, ['chameleon']),
+                    h('button', {
+                        attrs: {
+                            class: 'bg-gray-200 px-2 py-1'
+                        },
+                        on: {
+                            click: () => {
+                                _this.refreshItems = true
+                                _this.items = []
+                                chrome.runtime.sendMessage({
+                                    action: 'loadItemsFromDb',
+                                }, function (response) {
+                                    console.log(response, chrome.runtime.lastError);
+                                    _this.items = JSON.parse(response)
+                                    _this.refreshItems = false
+                                });
+                            }
+                        }
+                    }, ['刷新' + (_this.refreshItems ? '...' : '')])
+                ]),
+                h('div', {
+                    attrs: {
+                        class: 'items overflow-auto',
+                        style: 'height: calc(100% - 42px);'
+                    }
+                }, _this.items.map(item => {
+                    return h('div', {
+                        attrs: {
+                            class: 'item p-2 border-t'
                         }
                     }, [
                         h('div', {
+                                attrs: {
+                                    class: 'item text-base mb-1'
+                                }
+                            },
+                            [
+                                item.name,
+                                h('div', {
+                                    attrs: {
+                                        class: 'text-gray-500 text-sm'
+                                    }
+                                }, [item.host])
+                            ]
+                        ),
+                        h('div', {
                             attrs: {
-                                class: 'bg-green-50'
+                                class: 'hidden-doms'
                             }
-                        }, ['chameleon']),
-                        h('button', {
+                        }, item.hiddenDoms.map(hiddenDom => {
+                            return h('div', {
+                                attrs: {
+                                    class: 'hidden-dom flex items-center'
+                                }
+                            }, [h('input', {
+                                attrs: {
+                                    class: 'hidden-dom-checkbox mr-1',
+                                    type: 'checkbox',
+                                    checked: hiddenDom.checked,
+                                    id: item._id + hiddenDom.name
+                                },
+                                on: {
+                                    change(event) {
+                                        hiddenDom.checked = !!event.target.checked
+                                        _this.switchHiddenDom(event, hiddenDom, item)
+                                    }
+                                }
+                            }, []), h('label', {
+                                attrs: {
+                                    for: item._id + hiddenDom.name
+                                }
+                            }, [hiddenDom.name])])
+                        })),
+                        h('div', {
                             attrs: {
-                                class: 'bg-gray-200 px-2 py-1'
+                                class: 'styles'
+                            }
+                        }, (item.styles || []).map(style => {
+                            return h('div', {
+                                attrs: {
+                                    class: 'style flex items-center bg-green-50'
+                                }
+                            }, [h('input', {
+                                attrs: {
+                                    class: 'style-checkbox mr-1',
+                                    type: 'checkbox',
+                                    checked: style.checked,
+                                    id: item._id + style.name
+                                },
+                                on: {
+                                    change(event) {
+                                        style.checked = !!event.target.checked
+                                        _this.switchStyle(event, style, item)
+                                    }
+                                }
+                            }, []), h('label', {
+                                attrs: {
+                                    for: item._id + style.name
+                                }
+                            }, [style.name])])
+                        }))
+                    ])
+                }))
+            ])
+            const contentCreate = h('div', {
+                class: 'content-create h-full overflow-auto p-2'
+            }, [
+                h('div', {
+                    class: 'page'
+                }, [
+                    _this.page.title,
+                    h('div', {
+                        class: 'page-items'
+                    }, _this.page.list.map(item => {
+                        return h('div', {
+                            class: 'page-item',
+                        }, [h('input', {
+                            attrs: {
+                                class: 'hidden-dom-checkbox mr-1',
+                                type: 'checkbox',
+                                checked: false,
+                                id: item.insideId
                             },
                             on: {
-                                click: () => {
-                                    _this.refreshItems = true
-                                    _this.items = []
-                                    chrome.runtime.sendMessage({
-                                        action: 'loadItemsFromDb',
-                                    }, function (response) {
-                                        console.log(response, chrome.runtime.lastError);
-                                        _this.items = JSON.parse(response)
-                                        _this.refreshItems = false
+                                change(event) {
+                                    _this.switchHiddenDom(event, {
+                                        checked: !!event.target.checked,
+                                        selector: item.selector,
+                                        name: item.insideId
+                                    }, {
+                                        host: '',
+                                        name: '当前页面'
+                                    })
+                                }
+                            }
+                        }, []), h('label', {
+                            attrs: {
+                                for: item.insideId
+                            }
+                        }, [item.content])])
+                    }))
+                ])
+            ]);
+
+            return h(
+                'div', {
+                    class: 'w-80 h-96 pb-1 body flex items-stretch overflow-hidden'
+                },
+                [
+                    h('div', {
+                        class: "navs bg-gray-50 p-2 flex-none"
+                    }, [
+                        h('div', {
+                            class: "nav nav-mine cursor-pointer",
+                            on: {
+                                click() {
+                                    _this.currentNav = 'mine'
+                                }
+                            }
+                        }, ['我的']),
+                        h('div', {
+                            class: "nav nav-create cursor-pointer",
+                            on: {
+                                click() {
+                                    _this.currentNav = 'create'
+                                    chrome.tabs.query({
+                                        active: true,
+                                        currentWindow: true
+                                    }, function (tabs) {
+                                        chrome.tabs.sendMessage(
+                                            tabs[0].id, {
+                                                action: 'loadDocument'
+                                            },
+                                            function (response) {
+                                                // window.close();
+                                                _this.page = response;
+                                            }
+                                        );
                                     });
                                 }
                             }
-                        }, ['刷新' + (_this.refreshItems ? '...' : '')])
+                        }, ['创建'])
                     ]),
                     h('div', {
-                        attrs: {
-                            class: 'items'
-                        }
-                    }, _this.items.map(item => {
-                        return h('div', {
-                            attrs: {
-                                class: 'item p-2 border-t'
-                            }
-                        }, [
-                            h('div', {
-                                    attrs: {
-                                        class: 'item text-base mb-1'
-                                    }
-                                },
-                                [
-                                    item.name,
-                                    h('div', {
-                                        attrs: {
-                                            class: 'text-gray-500 text-sm'
-                                        }
-                                    }, [item.host])
-                                ]
-                            ),
-                            h('div', {
-                                attrs: {
-                                    class: 'hidden-doms'
-                                }
-                            }, item.hiddenDoms.map(hiddenDom => {
-                                return h('div', {
-                                    attrs: {
-                                        class: 'hidden-dom flex items-center'
-                                    }
-                                }, [h('input', {
-                                    attrs: {
-                                        class: 'hidden-dom-checkbox mr-1',
-                                        type: 'checkbox',
-                                        checked: hiddenDom.checked,
-                                        id: item._id + hiddenDom.name
-                                    },
-                                    on: {
-                                        change(event) {
-                                            hiddenDom.checked = !!event.target.checked
-                                            _this.switchHiddenDom(event, hiddenDom, item)
-                                        }
-                                    }
-                                }, []), h('label', {
-                                    attrs: {
-                                        for: item._id + hiddenDom.name
-                                    }
-                                }, [hiddenDom.name])])
-                            })),
-                            h('div', {
-                                attrs: {
-                                    class: 'styles'
-                                }
-                            }, (item.styles || []).map(style => {
-                                return h('div', {
-                                    attrs: {
-                                        class: 'style flex items-center bg-green-50'
-                                    }
-                                }, [h('input', {
-                                    attrs: {
-                                        class: 'style-checkbox mr-1',
-                                        type: 'checkbox',
-                                        checked: style.checked,
-                                        id: item._id + style.name
-                                    },
-                                    on: {
-                                        change(event) {
-                                            style.checked = !!event.target.checked
-                                            _this.switchStyle(event, style, item)
-                                        }
-                                    }
-                                }, []), h('label', {
-                                    attrs: {
-                                        for: item._id + style.name
-                                    }
-                                }, [style.name])])
-                            }))
-                        ])
-                    }))
+                        class: 'flex-auto h-full'
+                    }, [_this.currentNav == 'mine' ? contentMine : contentCreate])
                 ]
             )
         },
